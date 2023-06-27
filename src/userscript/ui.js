a8e1db464a713b0d813cc7fad0f05722a1835470
thoughtsunificator <oss@thoughtsunificator.me>
2023-06-27 03:07:50 +0200

    0.4.2

--- src/userscript/ui.js
+++ src/userscript/ui.js
@@ -1,6 +1,5 @@
 import IDMU from "../idmu/idmu.js"
 
-const dmUnsender = new IDMU(window)
 const button = document.createElement("button")
 button.textContent = "Unsend all DMs"
 button.style.position = "fixed"
@@ -9,7 +8,12 @@ button.style.right = "10px"
 button.style.zIndex = 9999
 button.addEventListener("click", async () => {
     button.disabled = true
+const dmUnsender = new IDMU(window)
+    if(!window.dmUnsender) {
+      window.dmUnsender = new IDMU(window)
+    }
     dmUnsender.unsendMessages()
+    
     button.disabled = false
   })
 
