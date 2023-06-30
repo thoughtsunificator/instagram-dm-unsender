d12e0dfa260a17978257f8fae06dcba99bd2aa22
thoughtsunificator <oss@thoughtsunificator.me>
2023-06-30 09:12:41 +0200

    .

--- src/instagram/instagram.js
+++ src/instagram/instagram.js
@@ -34,6 +34,7 @@ export default class Instagram {
 				new Promise(resolve => setTimeout(resolve, this.window.IDMU_MESSAGE_QUEUE_DELAY)).then(() => this.clearUnsendQueue())
 			}).catch(({error, task}) => {
 				console.error(error, `Task ${task.id}`)
+				new Promise(resolve => setTimeout(resolve, this.window.IDMU_MESSAGE_QUEUE_DELAY)).then(() => this.clearUnsendQueue())
 			})
 		}
 	}
