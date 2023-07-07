45880475dd96ccafe0ab169f541ff1cb08175e71
thoughtsunificator <oss@thoughtsunificator.me>
2023-07-08 00:42:01 +0200

    Add placeholder test for async-events module

---
+++ test/async-events.js
@@ -0,0 +1,10 @@
+import { test } from "./test.js"
+
+test("TODO waitForElement", async t => {
+	t.pass()
+})
+
+test("TODO clickElementAndWaitFor", async t => {
+	t.pass()
+})
+
--- test/wait-for-element.test.js
+++
@@ -1,6 +0,0 @@
-import { test } from "./test.js"
-
-test("waitForElement", async t => {
-	t.pass()
-})
-
