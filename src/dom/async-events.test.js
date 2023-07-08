a67ac678aae2ef215992214bad661f08fffab18c
thoughtsunificator <oss@thoughtsunificator.me>
2023-07-08 17:28:08 +0200

    Add test for async-events module

--- src/dom/async-events.test.js
+++ src/dom/async-events.test.js
@@ -1,10 +1,50 @@
 import { test } from "../../test/test.js"
+import { clickElementAndWaitFor, waitForElement } from "./async-events.js"
 
-test("TODO waitForElement", t => {
-	t.pass()
+test("waitForElement", async t => {
+	const element = t.context.document.createElement("p")
+	element.id = "test"
+	const promise = waitForElement(t.context.document.body, () => t.context.document.querySelector("#test"))
+	t.context.document.body.append(element)
+	const target = await promise
+	t.is(target.tagName, "P")
 })
 
-test("TODO clickElementAndWaitFor", t => {
-	t.pass()
+test("waitForElement #2", async t => {
+	const element = t.context.document.createElement("p")
+	element.id = "test"
+	const promise = waitForElement(t.context.document.body, () => t.context.document.querySelector("#test") !== null)
+	t.context.document.body.append(element)
+	const target = await promise
+	t.is(target, true)
+})
+
+test("waitForElement not found", async t => {
+	const element = t.context.document.createElement("p")
+	const promise = waitForElement(t.context.document.body, () => t.context.document.querySelector("#test") === null)
+	t.context.document.body.append(element)
+	const target = await promise
+	t.is(target, true)
+})
+
+test("clickElementAndWaitFor #2", async t => {
+	const element = t.context.document.createElement("p")
+	element.id = "test"
+	element.addEventListener("click", () => {
+		t.context.document.body.innerHTML += `<div id="sup432"></div>`
+	})
+	t.context.document.body.append(element)
+	const promise = clickElementAndWaitFor(element, t.context.document.body, () => t.context.document.querySelector("#sup432") !== null)
+	const target = await promise
+	t.is(target, true)
+})
+
+test("clickElementAndWaitFor not found", async t => {
+	const element = t.context.document.createElement("p")
+	element.id = "test"
+	t.context.document.body.append(element)
+	const promise = clickElementAndWaitFor(element, t.context.document.body, () => t.context.document.querySelector("#sup432") === null)
+	const target = await promise
+	t.is(target, true)
 })
 
